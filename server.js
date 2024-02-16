bdad8e78ce175eaf3c7d357afc0eb28a5e02a0e5
MarcioHideo <marcinishikawa@gmail.com>
2024-02-16 15:53:28 +0100

    Fixing errros

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ app.use(express.json())//allow server accept json
 // it's good to have 'v1' to show API version
 app.use("/api/v1/reviews", reviews)
 // "*" means anything else, in case user access a unknown route
-app.use("*", (req, res) => 
-    res.status(404).json({error: "not found"}))
+app.use("*", (req, res) => {
+    res.status(404).json({error: "not found"})})
 
 export default app
 
